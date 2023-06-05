2433c92a26c708ec2e0e49b3ce0da55be9c9e26d
matt-pasek <mpasek@tm1.edu.pl>
2023-06-05 20:11:37 +0200

    switch to mdx files

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,11 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import image from "@astrojs/image";
 import vue from "@astrojs/vue";
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), image({
     serviceEntryPoint: '@astrojs/image/sharp'
-  }), vue()]
+  }), vue(), mdx()]
 });
