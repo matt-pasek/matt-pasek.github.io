35c3a1754bb5393f16f8651644988908667e86ec
matt-pasek <mpasek@tm1.edu.pl>
2023-08-30 22:29:18 +0200

    updated to Astro 3.0

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,9 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-import image from "@astrojs/image";
 import vue from "@astrojs/vue";
 import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), image({serviceEntryPoint: '@astrojs/image/sharp'}), vue({ appEntrypoint: '/src/pages/_app' }), mdx()]
+  integrations: [tailwind(), vue({ appEntrypoint: '/src/pages/_app' }), mdx()]
 });
