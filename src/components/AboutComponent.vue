b1cdf2a1e40ff38a7def786dfa62c3040728a3e7
matt-pasek <mpasek@tm1.edu.pl>
2023-08-30 23:27:27 +0200

    about page first look

---
+++ src/components/AboutComponent.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="flex items-center justify-between">
+    <div>
+      <Card />
+    </div>
+    <div class="flex-col">
+      <Card />
+      <Card />
+      <Card />
+    </div>
+    <div>
+      <Card />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import Card from "./AboutComponentCard.vue";
+</script>
---
+++ src/components/AboutComponentCard.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="flex">
+  </div>
+</template>
+
+<script setup lang="ts">
+</script>
