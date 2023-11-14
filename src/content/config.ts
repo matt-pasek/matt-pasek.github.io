83a42436b46d3a073d108d1a97a61b14c3728f18
matt-pasek <mpasek@tm1.edu.pl>
2023-11-14 20:37:17 +0100

    added projects descriptions

--- src/content/config.ts
+++ src/content/config.ts
@@ -2,15 +2,16 @@ import { z, defineCollection } from 'astro:content';
 
 const projectsCollection = defineCollection({
   type: 'content',
-  schema: z.object({
-    codename: z.string().min(1),
-    image: z.string(),
-    positions: z.array(z.string()),
-    title: z.string(),
-    description: z.string(),
-    link: z.string().url(),
-    stack: z.array(z.string()),
-  }),
+  schema: ({ image }) =>
+    z.object({
+      codename: z.string().min(1),
+      image: image(),
+      positions: z.array(z.string()),
+      title: z.string(),
+      description: z.string(),
+      link: z.string().url(),
+      stack: z.array(z.string()),
+    }),
 });
 
 const recommendationsCollection = defineCollection({
